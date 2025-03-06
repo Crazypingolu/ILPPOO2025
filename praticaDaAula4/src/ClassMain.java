bedfe5e7e15b29a31c755a656c1ac9f13b8f7593
unknown <lucaswar05@gmail.com>
2025-03-06 17:03:33 -0300

    Aula 04 - Unique Instance

--- praticaDaAula4/src/ClassMain.java
+++ praticaDaAula4/src/ClassMain.java
@@ -1,8 +1,14 @@
 public class ClassMain {
     public static void main(String[] args) {
+        /*
         ClassPorteiro p1 = new ClassPorteiro("123", "Jose", 8);
         ClassPorteiro p2 = new ClassPorteiro("456", "Joao", 10);
         ClassPorteiro p3 = new ClassPorteiro("789", "Julio", 12);
+        */
+        ClassPorteiro p1 = ClassPorteiro.getInstanciado("123", "João", 8);
+        ClassPorteiro p2 = ClassPorteiro.getInstanciado("456", "José", 8);
 
+        System.out.println("p1: " + p1.nome);
+        System.out.println("p2: " + p2.nome); // não é josé por conta de porteiro ser único.
     }
 }
--- praticaDaAula4/src/ClassPorteiro.java
+++ praticaDaAula4/src/ClassPorteiro.java
@@ -2,13 +2,21 @@ public class ClassPorteiro {
     String num;
     String nome;
     int horaEnt;
-    
-    // const
-    public ClassPorteiro(String num, String nome, int horaEnt){
+    private static ClassPorteiro instanciado; // porteiro sendo único
+
+    // construtor
+    private ClassPorteiro(String num, String nome, int horaEnt){
         this.num = num;
         this.nome = nome;
         this.horaEnt = horaEnt;
     }
+    public static ClassPorteiro getInstanciado(String num, String nome, int horaEnt){ // condicional para construtor
+        if (instanciado == null)
+            instanciado = new ClassPorteiro(num, nome, horaEnt);
+        return instanciado;
+    }
+    
+    // Ações para "porteiro"
     public void abrirPortao(){}
     public void interfonar(){}
     public void atenderTelefone(){}
