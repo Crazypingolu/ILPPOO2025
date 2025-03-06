1736fcc737cf165fab2510dc26e0d7a6fba7c732
Lucas <lucaswar05@gmail.com>
2025-03-06 13:16:26 -0300

    Method 'venda' - now, it can add multiples values at once to 'saldo'

--- exerciciosPara0306/src/controller/ClasseController.java
+++ exerciciosPara0306/src/controller/ClasseController.java
@@ -6,9 +6,7 @@ public class ClasseController {
     }
     public void testeCaixa(){
         Caixa caixa1 = new Caixa();
-        double produto1 = 45.5, produto2 = 25.3, produto3 = 12.8;
-        System.out.printf("Venda concluída no valor de: %.2f %nSaldo atual do caixa 1: %.2f %n%n", produto1, caixa1.venda(produto1));
-        System.out.printf("Venda concluída no valor de: %.2f %nSaldo atual do caixa 1: %.2f %n%n", produto2, caixa1.venda(produto2));
-        System.out.printf("Venda concluída no valor de: %.2f %nSaldo atual do caixa 1: %.2f %n%n", produto3, caixa1.venda(produto3));
+        System.out.printf("Venda concluída, saldo atual do caixa 1: %.2f %n%n", caixa1.venda());
+
     }
 }
--- exerciciosPara0306/src/model/Caixa.java
+++ exerciciosPara0306/src/model/Caixa.java
@@ -1,5 +1,5 @@
 package model;
-
+import javax.swing.JOptionPane;
 public class Caixa {
     double saldo; // private
     
@@ -7,9 +7,14 @@ public class Caixa {
         this.saldo = 0;
     }
     
-    public double venda(double valorProduto){ 
-        double saldoAtual = this.saldo;
-        double saldoNovo = saldoAtual + valorProduto;
+    public double venda(){
+        double saldoAtual = this.saldo, saldoNovo = saldoAtual, valorProduto;
+        while(true){
+            valorProduto = Double.parseDouble(JOptionPane.showInputDialog("Adicionar valor de produto \n(digite 0 para cancelar)"));
+            if (valorProduto == 0)
+                break;
+            saldoNovo = saldoNovo + valorProduto;
+        }
         this.saldo = saldoNovo;
         return saldoNovo;
     }
