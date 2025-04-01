156bddde226e3aee62404d23a9ac1a2650fca893
unknown <lucaswar05@gmail.com>
2025-04-01 16:49:00 -0300

    Exe06 - Criado

---
+++ Exe06_InstanciasClasses/src/ClasseGarrafa.java
@@ -0,0 +1,18 @@
+public class ClasseGarrafa {
+   // Características:
+   public String cor;
+   public float tamanho;
+   public float espessura;
+   public float capacidadeDeLiquido;
+   public String material;
+   // Comportamentos:
+   public void receberLiquido(){
+       System.out.println("O líquido é colocado na garrafa");
+   }
+   public void manterLiquido(){
+       System.out.println("A garrafa contém o líquido");
+   }
+   public void liberarLíquido(){
+       System.out.println("O líquido deixa a garrafa");
+   } 
+}
